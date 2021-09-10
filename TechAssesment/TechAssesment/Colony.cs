7aa91d187f22550088d60458ae9646fa83f2456d
UltraViolet5 <70701763+UltraViolet5@users.noreply.github.com>
2021-09-10 19:17:11 +0200

    simulation

--- TechAssesment/TechAssesment/Colony.cs
+++ TechAssesment/TechAssesment/Colony.cs
@@ -95,5 +95,7 @@ namespace TechAssesment
         {
 
         }
+
+
     }
 }
---
+++ TechAssesment/TechAssesment/Simulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechAssesment
+{
+    class Simulation
+    {
+        public Simulation()
+        {
+            while (true)
+            {
+
+
+
+
+
+                var ch = Console.ReadKey(true);
+                if (ch.Key == ConsoleKey.Q || ch.Key == ConsoleKey.Enter)
+                {
+                    Environment.Exit(0);
+                };
+            }
+            
+        }
+
+    }
+}
